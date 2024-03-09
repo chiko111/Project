cec37a25d8ba5e9eeea2d46c5f05b7f8426019e9
Tsvetan Valchev <chiko.walchev@gmail.com>
2024-03-09 14:03:37 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route,Link,useLocation } from 'react-router-dom';
-import  TreeBuilder from './components/TreeBuilder/TreeBuilder'; 
-import TicTac from './components/TicTac/TicTac';
-import './Game.css';
-import './components/TreeBuilder/TreeBuilder.css'
-
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import TreeBuilder from "./components/TreeBuilder/TreeBuilder";
+import TicTac from "./components/TicTac/TicTac";
+import Gallery from "./components/Gallery/Gallery";
+import "./Game.css";
+import "./components/TreeBuilder/TreeBuilder.css";
 
 const RouteChangeListener = () => {
   const location = useLocation();
@@ -18,34 +24,35 @@ const RouteChangeListener = () => {
 };
 
 export default function App() {
-  
   return (
     <Router>
       <RouteChangeListener />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Game</Link>
-            </li>
-            <li>
-              <Link to="/input">Input</Link>
-            </li>
-            <li>
-              <Link to="/gallery">Gallery</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <TicTac />
-          </Route>
-          <Route path="/input">
-            <TreeBuilder />
-          </Route>
-          <Route path="/gallery">
-            <div><p>Gallery</p></div>
-          </Route>
-        </Switch>    
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Game</Link>
+          </li>
+          <li>
+            <Link to="/input">Input</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route
+          exact
+          path="/">
+          <TicTac />
+        </Route>
+        <Route path="/input">
+          <TreeBuilder />
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+      </Switch>
     </Router>
   );
 }
